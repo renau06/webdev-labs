7b35ef56990fbcd0e3173b600abe55a1266ed66b
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-05-03 23:46:29 -0400

    exercise 2 lab 6

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -5,3 +5,15 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const setTheme = (theme) => {
+
+    document.querySelector('body').className = theme;
+
+};
+
+document.querySelector("#default").addEventListener('click', ()=>setTheme('default'));
+document.querySelector("#desert").addEventListener('click', ()=>setTheme('desert'));
+document.querySelector("#ocean").addEventListener('click', ()=>setTheme('ocean'));
+document.querySelector("#high-contrast").addEventListener('click', ()=>setTheme('high-contrast'));
+
